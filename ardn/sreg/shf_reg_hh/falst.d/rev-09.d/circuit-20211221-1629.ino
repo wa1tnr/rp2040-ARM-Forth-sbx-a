44456c09ea2ee8d7192933048fc00025047ab45b
wa1tnr <wa1tnr@users.noreply.github.com>
2021-12-22 13:20:19 +0000

    Looking for subtle timing issues wrt program exec

--- ardn/sreg/shf_reg_hh/falst.d/rev-09.d/circuit-20211221-1629.ino
+++ ardn/sreg/shf_reg_hh/falst.d/rev-09.d/circuit-20211221-1629.ino
@@ -1,7 +1,4 @@
-// Wed 22 Dec 12:08:31 UTC 2021
-// Still trying for a good-timing AVR shift register program.
-
-// Tue 21 Dec 14:30:17 UTC 2021
+// Wed 22 Dec 13:18:23 UTC 2021
 
 // working program? captured
 
@@ -10,15 +7,22 @@ const int sr_clock = 3;
 const int sr_latch = 4;
 
 byte ledval = 0;
-
+const byte ledv_a = 119;
+/*
 void encode_ltr_a(void) { // A -- the letter, A
-    ledval = 1 + 2 + 4 + 16 + 32 + 64;
+    // ledval = 1 + 2 + 4 + 16 + 32 + 64;
+    ledval = 119;
 }
+*/
 
+const byte ledv_b = 124;
+/*
 void encode_ltr_b(void) { // b
     ledval = 4 + 8 + 16 + 32 + 64;
+    ledval = 124 ;
 }
-
+*/
+/*
 void encode_ltr_c(void) { // C
     ledval = 1 + 8 + 16 + 32;
 }
@@ -34,7 +38,7 @@ void encode_ltr_e(void) { // E
 void encode_ltr_f(void) { // F
     ledval = 1 + 0 + 16 + 32 + 64;
 }
-
+*/
 void encode_ltr_blank(void) { // blank
     ledval = 0 ;
 }
@@ -50,9 +54,11 @@ void clear_display(void) {
 
 void tell_display(void) {
   digitalWrite(sr_latch, LOW) ;
+  cli();
+  for (volatile int lag = 1288; lag > 0; lag--) { }
   shiftOut(sr_data, sr_clock, MSBFIRST, some_data);
   digitalWrite(sr_latch, HIGH);
-  for (volatile int count = 888; count > 0; count--) { }
+  for (volatile int count = 25888; count > 0; count--) { }
 }
 
 /*
@@ -68,14 +74,16 @@ void setup(void) {
 }
 
 void loop(void) {
-  encode_ltr_a();
-  some_data = ledval; // use encode words to fill ledval
+  // encode_ltr_a();
+  ledval = ledv_a;
+  some_data = ledv_a ; ledval; // use encode words to fill ledval
   tell_display();
 
-  encode_ltr_b();
+  // encode_ltr_b();
+  ledval = ledv_b;
   some_data = ledval;
   tell_display();
-
+/*
   encode_ltr_c();
   some_data = ledval;
   tell_display();
@@ -91,12 +99,12 @@ void loop(void) {
   encode_ltr_f();
   some_data = ledval;
   tell_display();
-
+*/
   encode_ltr_blank();
   some_data = ledval;
   tell_display();
 
 }
 
-// 22 December 2021 at 12:09 UTC
+// 22 December 2021 at 13:18 UTC
 // END.

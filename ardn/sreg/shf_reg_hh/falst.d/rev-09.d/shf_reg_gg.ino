e016381dd805ab389332e6d01baa491731f2b672
wa1tnr <wa1tnr@users.noreply.github.com>
2021-12-22 14:27:14 +0000

    Remove slowdowns in the code

--- ardn/sreg/shf_reg_hh/falst.d/rev-09.d/shf_reg_gg.ino
+++ ardn/sreg/shf_reg_hh/falst.d/rev-09.d/shf_reg_gg.ino
@@ -1,27 +1,16 @@
 // works with this circuit sim in this one context ;)
 
-// Refresh:
 // Wed 22 Dec 13:54:36 UTC 2021
-// Fri 10 Dec 17:36:17 UTC 2021
-// Sat Apr 27 22:25:53 UTC 2019
 
-// jeppifebbarkidia
+// zurkipfe
 
-// Originally:
-// Wed Nov 28 09:52:54 UTC 2018
-
-// finpaflik
-
-#define DATENOW     "Wed Nov 28 09:52:54 UTC 2018"
-// #include "config.h"
-// #include "src/periph/dotstar.h"
+#define DATENOW     "Wed Dec 22 14:10:57 UTC 2021"
 
 /*
 Adafruit Arduino - Lesson 4. 8 LEDs and a Shift Register
 Simon Monk mods: wa1tnr  27 November 2018
 */
 
-// Trinket M0
 int latchPin = 4;
 int clockPin = 3;
 int dataPin  = 2;
@@ -30,22 +19,12 @@ byte leds = 0;
 byte uleds = 0;
 byte pos = 15; // rightmost
 
-byte slew = 5;
+byte slew = 2;
 
 uint8_t ledval = 0;
 
 void setup(void) {
-    // setup_dotstar();
-
     Serial.begin(9600);
-
-    // while(!Serial) {
-
-    // for (int i = 4 ; i > 0 ; i--) {
-        // loop_dotstar(); // 4th iteration turns off dostar
-    // }
-    // Serial.print(DATENOW);
-
     pinMode(latchPin, OUTPUT);
     pinMode(dataPin,  OUTPUT);
     pinMode(clockPin, OUTPUT);
@@ -59,11 +38,8 @@ void _digitSelect(void) {
 void updateShiftRegister(void) {
     digitalWrite(latchPin, LOW);
     _digitSelect(); // digit 0 1 2 or 3 using 'pos' as the index
-
     uleds = leds;   // A-F 0-9 and a few other glyphs
-
     shiftOut(dataPin, clockPin, MSBFIRST, uleds); // paint the character's glyph!
-
     digitalWrite(latchPin, HIGH);
 }
 
@@ -73,48 +49,38 @@ void blankleds(void) {
 }
 
 #define EXPOSE_DIGIT_PAINTING 0
+
 void setleds(void) {
     leds = ledval;
     updateShiftRegister();
     if (!EXPOSE_DIGIT_PAINTING) {
         delay(1); // CRITICAL - must be a finite, non-zero delay here
     } else {
-        delay(400);
+        delay(4);
     }
 }
 
 void flash_digit(void) { // paint a single digit brightly, then immediately blank all LEDs
-    if (EXPOSE_DIGIT_PAINTING) {
-        // delay(122);
-        delay(424); // to expose digit change
-    }
     setleds();
     blankleds(); // waste no time in doing so!
 }
 
 #define REPETITIONS 2
+
 void in_column_zero(void) {
-    for (int i = REPETITIONS ; i>0; i--) {
         pos = 15 ; flash_digit();
-    }
 }
 
 void in_column_one(void) {
-    for (int i = REPETITIONS ; i>0; i--) {
         pos = 22 ; flash_digit();
-    }
 }
 
 void in_column_two(void) {
-    for (int i = REPETITIONS ; i>0; i--) {
         pos = 27 ; flash_digit();
-    }
 }
 
 void in_column_three(void) {
-    for (int i = REPETITIONS ; i>0; i--) {
        pos = 29 ; flash_digit();
-    }
 }
 
 // encodings - use powers of two added together, to show a 7-segment glyph
@@ -191,92 +157,66 @@ void encode_ltr_blank(void) { // blank
     ledval = 0 ;
 }
 
-
 // detailed messages to show on the 7-segment, 4-digit LED display:
 
 #define DURATION 2
 
 void msg_a_24(void) { // message:  'A824'
-    for (int j = 2;  j>0; j--) {
-        for (int k = DURATION; k>0; k--) {
             encode_four();   in_column_zero();
             encode_two();    in_column_one();
             encode_eight();  in_column_two();
             encode_ltr_a();  in_column_three();
-        }
-    }
     delay(10);
 }
 
 void msg_tttt(void) { // message: '3223'
-    for (int j = 2;  j>0; j--) {
-        for (int k = DURATION; k>0; k--) {
             //  columns 3 2 1 0  -- painted right to left!
             encode_three();  in_column_zero();   // print '3' in column '0'
             encode_two();    in_column_one();    // print '2' in column '1'
             encode_two();    in_column_two();    // print '2' in column '2'
             encode_three();  in_column_three();  // print '3' in column '3'
-        }
-    }
     delay(10);
 }
 
 void msg_le(void) { // message:  'LE  '
-    for (int j = 2;  j>0; j--) {
-        for (int k = DURATION; k>0; k--) {
             encode_ltr_blank();  in_column_zero();
             encode_ltr_blank();  in_column_one();
             encode_ltr_e();      in_column_two();
             encode_ltr_l();      in_column_three();
-        }
-    }
-    delay(10);
+    delay(2);
 }
 
 void msg_bef0(void) { // message: 'bEF0'
-    for (int j = 2;  j>0; j--) {
-        for (int k = DURATION; k>0; k--) {
             encode_zero();   in_column_zero();
             encode_ltr_f();  in_column_one();
             encode_ltr_e();  in_column_two();
             encode_ltr_b();  in_column_three();
-        }
-    }
-    delay(10);
+    delay(2);
 }
 
 void msg_foca(void) { // message: 'F0CA'
-    for (int j = 2;  j>0; j--) {
-        for (int k = DURATION; k>0; k--) {
             encode_ltr_a();  in_column_zero();
             encode_ltr_c();  in_column_one(); 
             encode_zero();   in_column_two();
             encode_ltr_f();  in_column_three();
-        }
-    }
-    delay(10);
+    delay(2);
 }
 
 void msg_cafe(void) { // message: 'CAFE'
-    for (int j = 2;  j>0; j--) {
-        for (int k = DURATION; k>0; k--) {
             encode_ltr_e();  in_column_zero();
             encode_ltr_f();  in_column_one();
             encode_ltr_a();  in_column_two();
             encode_ltr_c();  in_column_three();
-        }
-    }
-    delay(10);
+    delay(2);
 }
-
 
 
 void loop(void) {
     blankleds();
-    delay(4);
+    delay(2);
     ledval = 0;
     int i = 0;
-    delay(10);
+    delay(2);
 
  //   msg_tttt();
  //   msg_a_24();

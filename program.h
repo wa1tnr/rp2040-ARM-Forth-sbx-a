3c49131c97996c7010da2a444219260f2ba87fe7
wa1tnr <wa1tnr@users.noreply.github.com>
2022-01-01 20:45:58 +0000

    more dot-H file structure

--- program.h
+++ program.h
@@ -1,5 +1,8 @@
 // program.h
 // Mon Apr 26 01:06:59 UTC 2021
+#warning seen program.h
+#include "compatibility.h"
+#include "forth_defines.h"
 #define crlf(__a__); {Serial.print("\r\n");}
 
 extern void reflash_firmware(void); // prototype

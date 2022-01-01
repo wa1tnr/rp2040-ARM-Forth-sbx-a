cb3d4ce4f9fe28341e23603bc0072c5a2deca42a
wa1tnr <wa1tnr@users.noreply.github.com>
2022-01-01 20:47:07 +0000

    defines

---
+++ forth_defines.h
@@ -0,0 +1,7 @@
+#warning seen forth_defines.h
+
+#define RECENT_STAMP      "Sat  1 Jan 20:27:27 UTC 2022"
+#define COMMIT_TIME_STAMP "Sat Jan  1 17:44:52 UTC 2022"
+#define BRANCH_STAMP      "rp2040-dvlp-bb                      no releases"
+#define COMMIT_STAMP      "b9fe755" // seven characters
+#define FEATURE_STAMP     "+no_features                         "

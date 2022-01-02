dfc85e5ce7262df014e61c1a9f9e6deef774546e
wa1tnr <wa1tnr@users.noreply.github.com>
2022-01-02 18:16:05 +0000

    refinements to blinking stuff

--- main.fs
+++ main.fs
@@ -8,21 +8,6 @@ turnkey
 
 : test  ." this is going to be good " ;
 
-: wiggle blink blink blink blink blink ;
-
-\ literals
-
-: dlit dup 1+ ; \ simple and does what's wanted
-: jlit dlit dlit swap - ; \ always exactly '1'
-
-: nxt jlit + echo ; \ show next char
-: led 25 #, ;
-
-: blik led on 100 #, ms led off 800 #, ms ;
-
-: blinks ( n -- )
-  1- for blik next ." ok " ;
-
 : id ."  2 Jan Sunday   17:11z" cr
      ." rp2040-dvlp-dd-multc-a"cr ;
 
